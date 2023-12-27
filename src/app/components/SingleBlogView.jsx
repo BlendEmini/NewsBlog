5e4f4a6a5e881bb376b193076bb9c3251780ed3c
Blend Emini <blendxemini@gmail.com>
2023-12-27 11:03:43 +0100

    s

--- src/app/components/SingleBlogView.jsx
+++ src/app/components/SingleBlogView.jsx
@@ -4,7 +4,29 @@ const SingleBlogView = ({ singleBlogData }) => {
     console.log(singleBlogData);
 
     return (
+<<<<<<< HEAD
     
+=======
+        <>
+            {/* <Head>
+                <title>{currentPost.title}</title>
+                <meta
+                    name="description"
+                    content={currentPost.shortdescription}
+                />
+                <meta property="og:title" content={currentPost.title} />
+                <meta
+                    property="og:description"
+                    content={currentPost.shortdescription}
+                />
+                <meta property="og:image" content={currentPost.image} />
+                <meta
+                    property="og:url"
+                    content={`https://www.americanlensnews.com/blog/${currentPost.id}`}
+                />
+                <meta property="og:type" content="article" />
+            </Head> */}
+>>>>>>> parent of 25973a9 (trying to fix meta data)
             <div>
                 <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
                 <HamburgerMenu darkMode={darkMode} setDarkMode={setDarkMode} />
