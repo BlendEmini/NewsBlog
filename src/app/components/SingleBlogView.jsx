113032bab2f960c7da27410bc43b7db3b5506f8b
Blend Emini <blendxemini@gmail.com>
2023-12-27 11:02:30 +0100

    sadsad

--- src/app/components/SingleBlogView.jsx
+++ src/app/components/SingleBlogView.jsx
@@ -2,31 +2,9 @@ import SingleBlog from "./SingleBlog";
 
 const SingleBlogView = ({ singleBlogData }) => {
     console.log(singleBlogData);
-    if (!singleBlogData) {
-        return <div>Loading...</div>;
-    
-    return <SingleBlog singleBlogData={singleBlogData} />;
-=======
+
     return (
-        <>
-            {/* <Head>
-                <title>{currentPost.title}</title>
-                <meta
-                    name="description"
-                    content={currentPost.shortdescription}
-                />
-                <meta property="og:title" content={currentPost.title} />
-                <meta
-                    property="og:description"
-                    content={currentPost.shortdescription}
-                />
-                <meta property="og:image" content={currentPost.image} />
-                <meta
-                    property="og:url"
-                    content={`https://www.americanlensnews.com/blog/${currentPost.id}`}
-                />
-                <meta property="og:type" content="article" />
-            </Head> */}
+    
             <div>
                 <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
                 <HamburgerMenu darkMode={darkMode} setDarkMode={setDarkMode} />
@@ -35,7 +13,7 @@ const SingleBlogView = ({ singleBlogData }) => {
             </div>
         </>
     );
->>>>>>> parent of 25973a9 (trying to fix meta data)
+
 };
 
 export default SingleBlogView;
