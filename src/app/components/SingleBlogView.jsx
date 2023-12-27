25973a98296c08aaf76febbced9ec4505f4ab7c3
Blend Emini <blendxemini@gmail.com>
2023-12-27 10:11:08 +0100

    trying to fix meta data

--- src/app/components/SingleBlogView.jsx
+++ src/app/components/SingleBlogView.jsx
@@ -45,24 +45,6 @@ const SingleBlogView = () => {
 
     return (
         <>
-            {/* <Head>
-                <title>{currentPost.title}</title>
-                <meta
-                    name="description"
-                    content={currentPost.shortdescription}
-                />
-                <meta property="og:title" content={currentPost.title} />
-                <meta
-                    property="og:description"
-                    content={currentPost.shortdescription}
-                />
-                <meta property="og:image" content={currentPost.image} />
-                <meta
-                    property="og:url"
-                    content={`https://www.americanlensnews.com/blog/${currentPost.id}`}
-                />
-                <meta property="og:type" content="article" />
-            </Head> */}
             <div>
                 <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
                 <HamburgerMenu darkMode={darkMode} setDarkMode={setDarkMode} />
