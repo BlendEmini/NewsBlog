0b66ac79cd6eed2fee309816fd2088f55f2471ea
Blend Emini <blendxemini@gmail.com>
2024-02-07 21:52:58 +0100

    Update SingleViewPage.jsx

--- components/SingleViewPage.jsx
+++ components/SingleViewPage.jsx
@@ -15,85 +15,89 @@ const SingleViewPage = ({ singleBlogData }) => {
 
     return (
         <>
-            <div
-                className={`flex box-border align-items-center ${
-                    darkMode ? "bg-mainBgDark" : "bg-gray-100"
-                } flex-col justify-center`}
-            >
-                <div className="box-border py-4">
-                    <div className="flex box-border p-3 gap-4 max-w-3xl flex-col justify-center">
-                        <div className="flex w-full justify-between">
-                            <CategoryBtn category={singleBlogData.category} />
-                            <div>
-                                <h5
-                                    className={`text-sm w-28 font-semibold text-gray-400 ${
-                                        darkMode
-                                            ? "group-hover:text-white"
-                                            : "group-hover:text-mainBgDark"
-                                    }`}
-                                >
-                                    Created At:{" "}
-                                    {singleBlogData.created_at.slice(0, 10)}
-                                </h5>
+            <div>
+                <div
+                    className={`flex box-border align-items-center ${
+                        darkMode ? "bg-mainBgDark" : "bg-gray-100"
+                    } flex-col justify-center`}
+                >
+                    <div className="box-border py-4">
+                        <div className="flex box-border p-3 gap-4 max-w-3xl flex-col justify-center">
+                            <div className="flex w-full justify-between">
+                                <CategoryBtn
+                                    category={singleBlogData.category}
+                                />
+                                <div>
+                                    <h5
+                                        className={`text-sm w-28 font-semibold text-gray-400 ${
+                                            darkMode
+                                                ? "group-hover:text-white"
+                                                : "group-hover:text-mainBgDark"
+                                        }`}
+                                    >
+                                        Created At:{" "}
+                                        {singleBlogData.created_at.slice(0, 10)}
+                                    </h5>
+                                </div>
                             </div>
-                        </div>
-                        <div
-                            className={`${
-                                darkMode ? "text-white" : "text-mainBgDark"
-                            } md:text-4xl text-3xl font-semibold`}
-                        >
-                            {singleBlogData.title}
-                        </div>
-                        <div className="flex gap-2">
-                            <Image
-                                className="md:max-w-3xl box-border  border-radius-btn"
-                                width={800}
-                                height={100}
-                                priority={true}
-                                alt="IMG"
-                                src={singleBlogData.image}
-                            />
-                            <div className="hidden md:block">
-                                <AdsSidebar />
-                            </div>
-                        </div>
-                        <div className="box-border p-3">
-                            <h6
-                                className={` ${
-                                    darkMode
-                                        ? "text-white"
-                                        : "text-lightParagraphColor"
-                                } text-sm md:text-base font-normal font-family-single`}
+                            <div
+                                className={`${
+                                    darkMode ? "text-white" : "text-mainBgDark"
+                                } md:text-4xl text-3xl font-semibold`}
                             >
-                                {singleBlogData.shortdescription}
-                            </h6>
+                                {singleBlogData.title}
+                            </div>
+                            <div>
+                                <Image
+                                    className="md:max-w-3xl box-border  border-radius-btn"
+                                    width={800}
+                                    height={100}
+                                    priority={true}
+                                    alt="IMG"
+                                    src={singleBlogData.image}
+                                />
+                            </div>
+                            <div className="box-border p-3">
+                                <h6
+                                    className={` ${
+                                        darkMode
+                                            ? "text-white"
+                                            : "text-lightParagraphColor"
+                                    } text-sm md:text-base font-normal font-family-single`}
+                                >
+                                    {singleBlogData.shortdescription}
+                                </h6>
 
-                            {/* <h3 className="text-center font-bold py-5">
+                                {/* <h3 className="text-center font-bold py-5">
                                 Scroll Down to continue reading ⏬
                             </h3> */}
-                            {/* <AdsWidgetHeader /> */}
-                            <h6
-                                className={` ${
-                                    darkMode
-                                        ? "text-white"
-                                        : "text-lightParagraphColor"
-                                } text-sm md:text-base font-normal font-family-single`}
-                            >
-                                {singleBlogData.midDescription}
-                            </h6>
+                                {/* <AdsWidgetHeader /> */}
+                                <h6
+                                    className={` ${
+                                        darkMode
+                                            ? "text-white"
+                                            : "text-lightParagraphColor"
+                                    } text-sm md:text-base font-normal font-family-single`}
+                                >
+                                    {singleBlogData.midDescription}
+                                </h6>
 
-                            <h6
-                                className={` ${
-                                    darkMode
-                                        ? "text-white"
-                                        : "text-lightParagraphColor"
-                                } text-sm md:text-base font-normal font-family-single`}
-                            >
-                                {singleBlogData.description}
-                            </h6>
-                            <AdsWidgetInArticle />
+                                <h6
+                                    className={` ${
+                                        darkMode
+                                            ? "text-white"
+                                            : "text-lightParagraphColor"
+                                    } text-sm md:text-base font-normal font-family-single`}
+                                >
+                                    {singleBlogData.description}
+                                </h6>
+                                <AdsWidgetInArticle />
+                            </div>
                         </div>
                     </div>
+                </div>
+                <div className="hidden md:block">
+                    <AdsSidebar />
                 </div>
             </div>
         </>
