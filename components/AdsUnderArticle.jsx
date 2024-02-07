f63cbb1987d9613f70cbfac242583cd7b19f2733
Blend Emini <blendxemini@gmail.com>
2024-02-07 02:37:47 +0100

    returning as it was

--- components/AdsUnderArticle.jsx
+++ components/AdsUnderArticle.jsx
@@ -1,24 +1,14 @@
 "use client";
-import { useEffect, useState } from "react";
+import React from "react";
 
 const AdsUnderArticle = () => {
-    const [isClient, setIsClient] = useState(false);
-
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
-
     return (
         <>
-            {isClient && (
-                <>
-                    <div id="M939080ScriptRootC1582903"></div>
-                    <script
-                        src="https://jsc.adskeeper.com/a/m/americanlensnews.com.1582903.js"
-                        async
-                    ></script>
-                </>
-            )}
+            <div id="M939080ScriptRootC1582903"></div>
+            <script
+                src="https://jsc.adskeeper.com/a/m/americanlensnews.com.1582903.js"
+                async
+            ></script>
         </>
     );
 };
--- components/AdsWidgetInArticle.jsx
+++ components/AdsWidgetInArticle.jsx
@@ -1,24 +1,14 @@
 "use client";
-import { useEffect, useState } from "react";
+import React from "react";
 
 const AdsWidgetInArticle = () => {
-    const [isClient, setIsClient] = useState(false);
-
-    useEffect(() => {
-        setIsClient(true);
-    }, []);
-
     return (
-        <>
-            {isClient && (
-                <div id="M939080ScriptRootC1581687">
-                    <script
-                        src="https://jsc.adskeeper.com/a/m/americanlensnews.com.1581687.js"
-                        async
-                    ></script>
-                </div>
-            )}
-        </>
+        <div id="M939080ScriptRootC1581687">
+            <script
+                src="https://jsc.adskeeper.com/a/m/americanlensnews.com.1581687.js"
+                async
+            ></script>
+        </div>
     );
 };
 
