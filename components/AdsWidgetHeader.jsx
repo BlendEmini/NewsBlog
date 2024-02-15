d81b6b903a415a177f8c6041f509a507489e824e
Blend Emini <blendxemini@gmail.com>
2024-02-15 23:25:00 +0100

    removing under header and adding in article

--- components/AdsWidgetHeader.jsx
+++ components/AdsWidgetHeader.jsx
@@ -1,23 +1,13 @@
-"use client";
 import React from "react";
 
 const AdsWidgetHeader = () => {
     return (
         <>
-            <style>
-                {`
-          #M939080ScriptRootC1581685 {
-            min-height: 300px;
-          }
-        `}
-            </style>
-            {/* Composite Start */}
-            <div id="M939080ScriptRootC1581685"></div>
+            <div id="M939080ScriptRootC1586100"></div>
             <script
-                src="https://jsc.adskeeper.com/a/m/americanlensnews.com.1581685.js"
+                src="https://jsc.adskeeper.com/a/m/americanlensnews.com.1586100.js"
                 async
             ></script>
-            {/* Composite End */}
         </>
     );
 };
